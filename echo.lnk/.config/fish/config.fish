78e2ed81f0e9444d50bac08cb21b6c9cf165850a
Quanuanc <quanuanc@outlook.com>
2025-07-19 23:40:18 +0800

    echo: disable proxy

--- echo.lnk/.config/fish/config.fish
+++ echo.lnk/.config/fish/config.fish
@@ -2,9 +2,9 @@ if status is-interactive
   zoxide init fish | source
   atuin init fish --disable-up-arrow | source
 
-  set -gx http_proxy http://127.0.0.1:6154
-  set -gx https_proxy http://127.0.0.1:6154
-  set -gx all_proxy socks5://127.0.0.1:6155
+  # set -gx http_proxy http://127.0.0.1:6154
+  # set -gx https_proxy http://127.0.0.1:6154
+  # set -gx all_proxy socks5://127.0.0.1:6155
 
   function fish_greeting; end
 else
